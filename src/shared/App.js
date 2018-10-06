4481f663bcbc6a7a18dd85dcbc568f364ed53e25
Travis <travis@devmaterial.com>
2018-10-05 23:31:06 -0400

    first commit

---
+++ src/shared/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import Grid from './Grid'
+import routes from './routes'
+import { Route, Switch } from 'react-router-dom'
+import NoMatch from './NoMatch'
+import Navbar from './Navbar'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Switch>
+          {routes.map(({ path, exact, component: C, ...rest }) => (
+            <Route
+              key={path}
+              path={path}
+              exact={exact}
+              render={(props) => (
+                <C {...props} {...rest} />
+              )}
+            />
+          ))}
+          <Route render={(props) => <NoMatch {...props} />} />
+        </Switch>
+      </div>
+    )
+  }
+}
+
+export default App
